d16725d3dae1f7327d41bcaacf43397801fe4b09
Iamneo <techadmin@iamneo.ai>
2023-08-14 07:55:14 +0000

    Backup Commit

--- spring app/src/test/java/com/examly/springapp/SpringApplicationTests.java
+++ spring app/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -1,11 +1,13 @@
-package assignments;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
+package com.examly.springapp;
 import org.testng.annotations.Test;
-
+import java.net.URL;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.firefox.FirefoxDriver;
 public class Assignment1 {
 
 	// Creating an instance of Firefox Browser
