25f849cb8a6803a229d7f3a6d002cabfac856d6b
Iamneo <techadmin@iamneo.ai>
2023-08-14 08:36:12 +0000

    Backup Commit

--- spring app/src/test/java/com/examly/springapp/SpringApplication.java
+++ spring app/src/test/java/com/examly/springapp/SpringApplication.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 import org.testng.Assert;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.firefox.FirefoxDriver;
-public class springapp{
+public class SpringApplication{
 
 	// Creating an instance of Firefox Browser
 	FirefoxDriver driver;
